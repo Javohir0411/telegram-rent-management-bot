521d92b5c0046f3c440ad871cb6fcdeadb13de82
Javohir0411 <javohirabduhakimov979@gmail.com>
2026-03-04 13:41:41 +0500

    Change main.py

--- main.py
+++ main.py
@@ -6,18 +6,26 @@ from aiogram.enums import ParseMode
 from routers import router as main_router
 import asyncio
 import logging
-import db
+from database.base import Base
 from database.config import settings
 from routers.rent_process.send_daily_notifications import send_expired_rent_notification
 
 logging.basicConfig(level=logging.INFO)
 
 
+async def init_db():
+    # Jadvalarni yaratish (agar mavjud bo‘lmasa)
+    engine = create_async_engine(settings.DATABASE_URL, echo=False)
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    await engine.dispose()
+
+
 async def schedule_daily(bot: Bot):
     """Tonggi 7 da har kuni send_expired_rent_notifications chaqirish."""
     while True:
         now = datetime.now()
-        target = datetime.combine(now.date(), time(2, 0))  # uzb vaqti bo'yicha bugungi 7:00
+        target = datetime.combine(now.date(), time(7, 0))  # uzb vaqti bo'yicha bugungi 7:00
         if now >= target:
             target += timedelta(days=1)  # agar 7:00dan keyin bo'lsa, ertaga
         sleep_seconds = (target - now).total_seconds()
@@ -30,6 +38,9 @@ async def schedule_daily(bot: Bot):
 
 
 async def main():
+
+    await init_db()
+
     bot = Bot(
         token=settings.bot_token,
         default=DefaultBotProperties(parse_mode=ParseMode.HTML)
